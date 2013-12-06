8563a16ef72f381f73becb5f4b9c1c0a93c38ff0
Jon Kuperman <jon.kuperman@gmail.com>
2013-12-05 22:48:02 -0500

    adds facebook login and basic styling

--- client/views/idealist/idealist.js
+++ client/views/idealist/idealist.js
@@ -9,10 +9,8 @@ define('ideaListView', function() {
             display_root_ideas: function() {
                 // Ideas.insert({"_id": new Meteor.Collection.ObjectID().toHexString(), name: 'level one', children: [{"_id": new Meteor.Collection.ObjectID().toHexString(), name: 'level two', children: [{name: 'level three', children: {}}]}]});
             }
-        }
+        };
     }());
-
-    // Template.ideaList.rendered = 
 
     Template.ideaList.helpers({
         root_ideas: ideaListView.get_root_ideas.bind(ideaListView)
