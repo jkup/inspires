fd4de1cdd67d34d8f33866c4a2978bd1a52347b3
Rafael Corral <rxcorral@gmail.com>
2013-12-07 22:38:29 -0500

    New ideas can now be added anywhere in the tree!

--- client/views/idealist/idealist.js
+++ client/views/idealist/idealist.js
@@ -1,22 +1,38 @@
+// Judgement free zone...wrote this while drinking wine
 define('ideaListView', ['_Idea'], function(Idea) {
     String.prototype.repeat = function(num) {
         return new Array(num + 1).join(this);
     }
 
-    // Setup listeners
-    jQuery(document).on('click', '[data-behavior~=add-idea]', function() {
-        var idea_title = prompt('Idea title:')
-            , $this = jQuery(this)
-            , objectId = $this.data('id')
-            ;
+    // Outer most selector
+    jQuery(document)
 
-        // See if we are adding a root idea or child idea
-        if (0 === objectId) {
-            ideaListView.add_root(idea_title);
-        } else {
-            ideaListView.add_child(objectId, idea_title);
-        }
-    });
+        // Setup DOM listeners
+        .on('click', '[data-behavior~=add-idea]', function() {
+            jQuery(this).trigger('add_idea');
+        })
+        .on('click', '[data-behavior~=expand-idea]', function() {
+            jQuery(this).trigger('expand_idea');
+        })
+
+        // Setup custom events
+        .on('add_idea.idea_list', function(e) {
+            var idea_title = prompt('Idea title:')
+                ,$this = jQuery(e.target)
+                ,objectId = $this.data('id')
+                ;
+
+            // See if we are adding a root idea or child idea
+            if (0 === objectId) {
+                ideaListView.add_root(idea_title);
+            } else {
+                ideaListView.add_child(objectId, idea_title);
+            }
+        })
+        .on('expand_idea.idea_list', function(e) {
+            jQuery(e.target).siblings('ul').slideDown();
+        })
+        ;
 
     var ideaListView = (function() {
         // This is here until this happens: https://jira.mongodb.org/browse/SERVER-831
@@ -24,24 +40,49 @@ define('ideaListView', ['_Idea'], function(Idea) {
 
         return {
             get_root_ideas: function() {
-                return Ideas.find({}, {fields: {title: 1}});
+                var _ideas = Ideas.find()
+                    ,that = this
+                    ;
+
+                // Run in background
+                setTimeout(function() {
+                    var ideas = _ideas.fetch();
+                    for (var i = 0; i < ideas.length; i++) {
+                        that.build_paths_recursively(ideas[i], ideas[i]._id);
+                    };
+                });
+
+                return _ideas;
             },
 
             get_root_idea: function(objectId) {
                 return Ideas.findOne({_id: objectId});
             },
 
+            build_paths_recursively: function() {
+                var args = _.extend([
+                    null, // idea
+                    null, // root_id
+                    0, // depth
+                    'children' // push_path
+                    ], arguments);
+
+                // Add root path
+                paths[args[0]._id] = {
+                    depth: args[2]
+                    ,root_id: args[1]
+                    ,push_path: args[3]
+                };
+
+                for (var i = 0; i < args[0].children.length; i++) {
+                    this.build_paths_recursively(args[0].children[i], args[1], args[2] + 1, args[3] + '.' + i + '.children');
+                };
+            },
+
             get_path_to_object: function(objectId) {
                 // If path to object isn't set then build from db
                 if (!paths[objectId]) {
-                    var root = this.get_root_idea(objectId);
-
-                    // Add root path
-                    paths[root._id] = {
-                        depth: 0,
-                        find_path: '_id',
-                        push_path: 'children'
-                    };
+                    this.build_paths_recursively(this.get_root_idea(objectId), objectId);
                 }
 
                 return paths[objectId];
@@ -53,88 +94,30 @@ define('ideaListView', ['_Idea'], function(Idea) {
 
             add_child: function(objectId, idea_title) {
                 var path = this.get_path_to_object(objectId)
-                    , find = {}
-                    , push = {}
+                    ,find = {}
+                    ,push = {}
                     ;
 
                 if (!path) throw 'ObjectId not found.';
 
-                find[path.find_path] = new Meteor.Collection.ObjectID(objectId).toHexString();
+                // find[path.find_path] = new Meteor.Collection.ObjectID(objectId).toHexString();
                 push[path.push_path] = new Idea({title: idea_title});
 
-                Ideas.update(find, {$push: push});
+                Ideas.update({'_id': new Meteor.Collection.ObjectID(path.root_id).toHexString()}, {$push: push});
+            },
+
+            get_paths: function() {
+                return paths;
             },
 
             initialize: function() {}
         };
     }());
-
+window.ideaListView = ideaListView;
     Template.ideaList.helpers({
         root_ideas: ideaListView.get_root_ideas.bind(ideaListView)
-        , root_id: function(){
+        ,root_id: function(){
             return _.extend({root_id: this.root_id || this._id}, this);
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // To allow content editable shiz in the future...
-    function placeCaretAtEnd(el) {
-        el.focus();
-        if (typeof window.getSelection != 'undefined' && typeof document.createRange != 'undefined') {
-            var range = document.createRange();
-            range.selectNodeContents(el);
-            range.collapse(false);
-            var sel = window.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-        } else if (typeof document.body.createTextRange != 'undefined') {
-            var textRange = document.body.createTextRange();
-            textRange.moveToElementText(el);
-            textRange.collapse(false);
-            textRange.select();
-        }
-    }
