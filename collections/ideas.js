a569aac5a5ff1403269230b1ebb7ef999bb407b4
Kelly King <kelly.danielle.king@gmail.com>
2013-12-15 02:13:16 -0500

    change the style of the main add button

--- collections/ideas.js
+++ collections/ideas.js
@@ -1,13 +1,13 @@
 Ideas = new Meteor.SmartCollection('ideas');
 
 Ideas.allow({
-    insert: function() {
-        if (this.userId) {
-            return true;
-        } else {
-            return false;
-        }
-    },
+    // insert: function() {
+    //     if (this.userId) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // },
     update: function() {
     	// Only allow if we are adding something to the children array
     	if (this.userId && arguments[2].length == 1 && arguments[2][0] == 'children' && Object.size(arguments[3]) && arguments[3]['$push']) {
