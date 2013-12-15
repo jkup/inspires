cae0cc2318055ce2ec7e8d9a697da9012cfc9b0d
Kelly King <kelly.danielle.king@gmail.com>
2013-12-15 17:09:31 -0500

    adding facebook and twitter login images

--- collections/ideas.js
+++ collections/ideas.js
@@ -10,7 +10,7 @@ Ideas.allow({
     // },
     update: function() {
     	// Only allow if we are adding something to the children array
-    	if (this.userId && arguments[2].length == 1 && arguments[2][0] == 'children' && Object.size(arguments[3]) && arguments[3]['$push']) {
+    	if (arguments[2].length == 1 && arguments[2][0] == 'children' && Object.size(arguments[3]) && arguments[3]['$push']) {
     		return true;
     	} else {
     		return false;
