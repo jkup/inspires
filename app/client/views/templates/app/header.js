2bcdcada970fb7b3eaba313f0db71386cf6aa91a
Rafael Corral <rxcorral@gmail.com>
2014-01-18 20:30:54 -0500

    Updated login area to match mocks. Fixes to arrow on the left of ideas.

---
+++ app/client/views/templates/app/header.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Template._loginButtonsLoggedOutAllServices.helpers({
+	services: function() {
+		var services = Accounts.oauth.serviceNames()
+			,_return = []
+			,capitalized = ''
+			;
+
+		for (var i = services.length - 1; i >= 0; i--) {
+			capitalized = services[i].substr(1);
+
+			_return.push({
+				configured: true
+				,name: services[i]
+				,capitalizedName: services[i][0].toUpperCase() + capitalized
+			});
+		}
+
+		return _return;
+	}
+});
+
+Template._loginButtonsLoggedIn.helpers({
+	dropdown: false
+});
+
+Template._loginButtonsLoggedInSingleLogoutButton.rendered = function() {
+	jQuery('#login-buttons-logout').html('Logout');
+};
